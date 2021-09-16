70971acb5b40a72b071554aedd86b24042ccd309
Cenk Akay <cenk_akay08@hotmail.com>
2021-09-16 12:17:46 +0300

    added .env file for security

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/firestore";
 
 const config = {
-  apiKey: "AIzaSyAUDDlywLQS_306UGyJoiP1CQ4JQorDe3A",
-  authDomain: "rota-teknik-case.firebaseapp.com",
-  projectId: "rota-teknik-case",
-  storageBucket: "rota-teknik-case.appspot.com",
-  messagingSenderId: "315338158705",
-  appId: "1:315338158705:web:26324a1b218784131d21d0",
+  apiKey: process.env.REACT_APP_APIKEY,
+  authDomain: process.env.REACT_APP_AUTDOMAIN,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_APPID,
 };
 // Initialize Firebase
 const fire = firebase.initializeApp(config);
